e79a989ee47c2429695940683c808e0cd36885ae
kevinhes <kevinhes1115@gmail.com>
2025-01-20 10:43:40 +0800

    設定 BrowserRouter 的基本路徑

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/gallery-ecommerce">
     <App />
   </BrowserRouter>
   // <StrictMode>
