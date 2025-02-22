90a348608dccd59f5574a9e8eab4494e7fb21258
kevinhes <kevinhes1115@gmail.com>
2025-02-22 12:57:43 +0800

    新增購物車功能，包含編輯產品數量及清空購物車的功能，並新增購物車列表元件

--- src/helpers/shoppingCart.js
+++ src/helpers/shoppingCart.js
@@ -32,9 +32,33 @@ export const getShoppingCart = async() => {
   try {
     const response = await axios.get(addCartUrl)
     const { carts } = response.data.data
-    console.log(carts);
     return carts
     
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+export const editProductQty = async(product_id, qty) => {
+  const editCartUrl = `${baseUrl}v2/api/${api}/cart/${product_id}`
+  try {
+    const response = await axios.put(editCartUrl, {
+      data: {
+        product_id,
+        qty
+      }
+    })
+    if( response.data.success ) {
+      await Swal.fire({
+        title: '產品數量已調整',
+        icon: 'success',
+        timer: 1500,
+        showConfirmButton: false,
+      })
+    }
+    const carts = await getShoppingCart()
+    return carts
   } catch (error) {
     console.log(error);
     
@@ -48,6 +72,26 @@ export const deleteProductFromShoppingCart = async(id) => {
     if( response.data.success ) {
       await Swal.fire({
         title: '產品已刪除',
+        icon: 'success',
+        timer: 1500,
+        showConfirmButton: false,
+      })
+    }
+    const carts = await getShoppingCart()
+    return carts
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+export const deleteAllProduct = async() => {
+  const deleteCartUrl = `${baseUrl}v2/api/${api}/carts`
+  try {
+    const response = await axios.delete(deleteCartUrl)
+    if( response.data.success ) {
+      await Swal.fire({
+        title: '購物車已清空',
         icon: 'success',
         timer: 1500,
         showConfirmButton: false,
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,17 +1,43 @@
 import { useEffect, useState } from "react"
 import { useForm } from 'react-hook-form';
+import ReactLoading from 'react-loading';
+import Swal from 'sweetalert2'
 
+// custome func library
+import { getShoppingCart, deleteProductFromShoppingCart, checkout, editProductQty, deleteAllProduct } from "../helpers/shoppingCart"
 
-import { getShoppingCart, deleteProductFromShoppingCart, checkout } from "../helpers/shoppingCart"
+//components
+import ShoppingCartList from "../components/shoppingcart/ShoppingCartList";
 
+// page
 export default function ShoppingCart() {
   const [ shoppingCart, setShoppingCart ] = useState([])
+  const [ isCartLoading, setIsCartLoading ] = useState(false)
+  const [ isOrderLoading, setIsOrderLoading ] = useState(false)
 
   const { register, handleSubmit, formState: { errors } } = useForm();
 
   const handleShoppingCart = async() => {
+    setIsCartLoading(true)
     const shoppingCartDetail = await getShoppingCart()
     setShoppingCart(shoppingCartDetail)
+    setIsCartLoading(false)
+  }
+
+  const handleEditShoppingCart = async(product_id, qty) => {
+    if ( qty <= 0 ) {
+      await Swal.fire({
+        title: '產品數量不得為零',
+        icon: 'warning',
+        timer: 1500,
+        showConfirmButton: false,
+      })
+      return
+    }
+    setIsCartLoading(true)
+    const shoppingCartDetail = await editProductQty(product_id, qty)
+    setShoppingCart(shoppingCartDetail)
+    setIsCartLoading(false)
   }
 
   const deleteProduct = async( id ) => {
@@ -19,9 +45,18 @@ export default function ShoppingCart() {
     setShoppingCart(shoppingCartDetail)
   }
 
+  const deleteAllProductFromCart = async() => {
+    setIsCartLoading(true)
+    const shoppingCartDetail = await deleteAllProduct()
+    setShoppingCart(shoppingCartDetail)
+    setIsCartLoading(false)
+  }
+
   const checkoutOrder = async(data) => {
+    setIsOrderLoading(true)
     const shoppingCartDetail = await checkout(data)
     setShoppingCart(shoppingCartDetail)
+    setIsOrderLoading(false)
   }
 
   useEffect(()=> {
@@ -30,115 +65,111 @@ export default function ShoppingCart() {
 
   return (
     <section className="container">
-      <div className="row">
-        <div className="col-8">
-          <ul className="list-unstyled">
+      {
+        isOrderLoading === true &&
+        <div className="full-screen-loading">
+          <ReactLoading />
+        </div>
+      }
+      {
+        shoppingCart.length === 0 ? 
+        <div>
+          <p className="h1">購物車內無商品</p>
+        </div>
+        :
+        <div className="row">
+          <div className="col-12 d-flex justify-content-end mb-5">
+            <button type="button" className="btn btn-danger" onClick={() => deleteAllProductFromCart() }>
+              清空購物車
+            </button>
+          </div>
+          <div className="col-8">
             {
-              shoppingCart.map( ( cartItem ) => {
-                return (
-                  <li key={cartItem.id} className="shopping-cart-card">
-                    <div className="card-img-wrap">
-                      <img src={cartItem.product.imageUrl} alt="product image" className="w-100" />
-                    </div>
-                    <div className="d-flex align-items-center flex-grow-1">
-                      <div className="w-70">
-                        <h5 className="mb-2">
-                          {cartItem.product.title}
-                        </h5>
-                        <p>
-                        {cartItem.product.content}
-                        </p>
-                      </div>
-                      <div className="w-10">
-                        <h4>
-                        {cartItem.qty}
-                        </h4>
-                      </div>
-                      <div className="w-15">
-                        <h4>${cartItem.final_total}</h4>
-                      </div>
-                      <div className="flex-grow-1">
-                        <button type="button" onClick={() => deleteProduct(cartItem.id)}>
-                          <i className="bi bi-trash fs-3 text-danger"></i>
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                )
-              } )
+              isCartLoading === true ?
+              <div className="w-100 vh-100 d-flex justify-content-center align-items-center">
+                <ReactLoading type="spin" color="#4F46E5" />
+              </div>
+              :
+              <ShoppingCartList
+                shoppingCart={shoppingCart}
+                deleteProduct={deleteProduct}
+                handleEditShoppingCart={handleEditShoppingCart}
+                deleteAllProductFromCart = {deleteAllProductFromCart}
+              />
             }
-          </ul>
-        </div>
-        <div className="col-4">
-          <form onSubmit={handleSubmit(checkoutOrder)} className="order-form">
-            <div className="form-floating mb-3">
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                placeholder="姓名"
-                {...register('name', {
-                  required: '姓名是必填的',
-                })}
-              />
-              <label htmlFor="username">姓名</label>
-            </div>
-            {errors.name && <p className="text-danger">{errors.name.message}</p>}
-            <div className="form-floating mb-3">
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="Email"
-                {...register('email', {
-                  required: 'Email 是必填的',
-                  pattern: {
-                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                    message: '請輸入有效的 Email',
-                  },
-                })}
-              />
-              <label htmlFor="email">Email</label>
-            </div>
-            {errors.email && <p className="text-danger">{errors.email.message}</p>}
-            <div className="form-floating mb-3">
-              <input
-                type="tel"
-                className="form-control"
-                id="tel"
-                placeholder="手機號碼"
-                {...register('tel', {
-                  required: '手機號碼是必填的',
-                  pattern: {
-                    value: /^09\d{8}$/,  // 台灣手機號碼驗證
-                    message: '請輸入有效的台灣手機號碼（09 開頭，共 10 碼）',
-                  },
-                })}
-              />
-              <label htmlFor="tel">手機號碼</label>
-            </div>
-            {errors.tel && <p className="text-danger">{errors.tel.message}</p>}
-            <div className="form-floating mb-3">
-              <input
-                type="text"
-                className="form-control"
-                id="address"
-                placeholder="地址"
-                {...register('address', {
-                  required: '地址是必填的',
-                })}
-              />
-              <label htmlFor="address">地址</label>
-            </div>
-            {errors.address && <p className="text-danger">{errors.address.message}</p>}
-            <div className="form-floating mb-3">
-              <textarea className="form-control floating-area" placeholder="留言" id="message" { ...register('message') }></textarea>
-              <label htmlFor="message">留言</label>
-            </div>
-            <button type="submit" className="btn btn-primary w-100">訂單送出</button>
-          </form>
-        </div>
+          </div>
+          <div className="col-4">
+            <form onSubmit={handleSubmit(checkoutOrder)} className="order-form">
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  placeholder="姓名"
+                  {...register('name', {
+                    required: '姓名是必填的',
+                  })}
+                />
+                <label htmlFor="username">姓名</label>
+              </div>
+              {errors.name && <p className="text-danger">{errors.name.message}</p>}
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Email"
+                  {...register('email', {
+                    required: 'Email 是必填的',
+                    pattern: {
+                      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                      message: '請輸入有效的 Email',
+                    },
+                  })}
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+              {errors.email && <p className="text-danger">{errors.email.message}</p>}
+              <div className="form-floating mb-3">
+                <input
+                  type="tel"
+                  className="form-control"
+                  id="tel"
+                  placeholder="手機號碼"
+                  {...register('tel', {
+                    required: '手機號碼是必填的',
+                    pattern: {
+                      value: /^09\d{8}$/,  // 台灣手機號碼驗證
+                      message: '請輸入有效的台灣手機號碼（09 開頭，共 10 碼）',
+                    },
+                  })}
+                />
+                <label htmlFor="tel">手機號碼</label>
+              </div>
+              {errors.tel && <p className="text-danger">{errors.tel.message}</p>}
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="address"
+                  placeholder="地址"
+                  {...register('address', {
+                    required: '地址是必填的',
+                  })}
+                />
+                <label htmlFor="address">地址</label>
+              </div>
+              {errors.address && <p className="text-danger">{errors.address.message}</p>}
+              <div className="form-floating mb-3">
+                <textarea className="form-control floating-area" placeholder="留言" id="message" { ...register('message') }></textarea>
+                <label htmlFor="message">留言</label>
+              </div>
+              <button type="submit" className="btn btn-primary w-100">訂單送出</button>
+            </form>
+          </div>
       </div>
+      }
+
     </section>
   )
 }
