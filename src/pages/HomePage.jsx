b49f5d34887e4a4353b739117534362d73f06961
kevinhes <kevinhes1115@gmail.com>
2025-03-05 16:51:36 +0800

    完成首頁第一段 新增字型樣式，更新 SCSS 檔案，調整前端產品切片狀態管理，並改善首頁結構與樣式

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,16 +8,52 @@ export default function HomePage() {
   });
   const dispatch = useDispatch()
   const backgroundImage = '/images/Tsunami_by_hokusai_19th_century.jpg';
-  
+
 
   useEffect(() => {
     dispatch(getFrontProductsListByPage())
-  },[])
+  }, [])
 
   return (
     <main className="position-relative">
-      <section className="position-absolute top-0 start-0 vw-100 vh-100 overflow-hidden opacity-50">
-        <img src={backgroundImage} alt="背景圖片" className="bg-image" />
+      {/* 第一張投影片 */}
+      <section className="vw-100 vh-100 position-relative">
+        {/* 畫作 */}
+        <ul className="position-absolute w-100 h-100 list-unstyled z-index-100">
+          {productsList.map(((paintings, index) => (
+            index <= 2 ? (
+              <li key={paintings.id} className="card rotate-card">
+                <img src={paintings.imageUrl} alt={paintings.title} />
+              </li>
+            ) : null
+          )))}
+        </ul>
+        {/* 標語 */}
+        <div className="text-center position-relative z-index-100 top-85">
+          <h2 className="mb-3 h1">
+            讓每一幅畫，成為你生活的靈感泉源
+          </h2>
+          <h1 className="federo">ArtNova</h1>
+        </div>
+        {/* 背景讀片 */}
+        <section className="position-absolute top-0 start-0 vw-100 vh-100 overflow-hidden opacity-25 z-index-1">
+          <img src={backgroundImage} alt="背景圖片" className="bg-image" />
+        </section>
+      </section>
+      {/* 關於我們 */}
+      <section className="py-20">
+        <div className="container">
+          <h2 className="text-center mb-8">
+            關於我們
+          </h2>
+          <div className="row">
+            <div className="offset-2 col-8">
+              <div className="about-card">
+                
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
--- src/slice/frontendProductsSlice.js
+++ src/slice/frontendProductsSlice.js
@@ -3,7 +3,11 @@ const baseUrl = import.meta.env.VITE_API_BASE_URL;
 const api = import.meta.env.VITE_API_PATH;
 import axios from 'axios';
 
-const initialState = { productsList: [] };
+const initialState = {
+  productsList: [],
+  pagination: {},
+  isPageChanged: false,
+};
 
 export const frontendProductsSlice = createSlice({
   name: 'frontendProducts',
@@ -12,6 +16,12 @@ export const frontendProductsSlice = createSlice({
     setProducts(state, { payload }) {
       state.productsList = payload
     },
+    setPagination(state, { payload }) {
+      state.pagination = payload
+    },
+    setIsPageChanged(state, { payload }) {
+      state.isPageChanged = payload
+    }
   },
 });
 
@@ -19,18 +29,22 @@ export const getFrontProductsListByPage = createAsyncThunk(
   'frontendProducts/getFrontProductsListByPage',
   async({ page = 1, maxPage = Infinity } = {}, { dispatch }) => {
     const getProductsUrl = `${baseUrl}v2/api/${api}/products?page=${page}`;
+    
     if (page === 0 || page > maxPage) return;
     try {
       const response = await axios.get(getProductsUrl);
       const { products } = response.data;
+      const { pagination } = response.data;
       console.log(products);
+      
       dispatch(setProducts(products))
+      dispatch(setPagination(pagination))
     } catch (error) {
       alert(error.response.data.message);
     }
   }
 )
 
-export const { setProducts } = frontendProductsSlice.actions;
+export const { setProducts, setPagination, setIsPageChanged } = frontendProductsSlice.actions;
 
 export default frontendProductsSlice.reducer;
